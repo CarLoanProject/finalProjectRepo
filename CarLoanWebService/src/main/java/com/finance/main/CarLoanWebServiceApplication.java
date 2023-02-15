de2c08e335199398c2cb307bacc49b0520b559e7
Vilas <vilasthakare777@gmail.com>
2023-02-15 12:54:59 +0530

    initial commit

---
+++ CarLoanWebService/src/main/java/com/finance/main/CarLoanWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.finance.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarLoanWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarLoanWebServiceApplication.class, args);
+	}
+
+}
